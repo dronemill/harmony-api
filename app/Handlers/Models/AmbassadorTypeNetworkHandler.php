153678dec7fc134e2a1066fd6b0afa406325b250
Patrick McCarren <patrick@mmsc.io>
2015-04-01 00:58:35 -0400

    app.handlers: inital checkin of model handlers

---
+++ app/Handlers/Models/AmbassadorTypeNetworkHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Handlers\Models;
+
+// use Symfony\Component\HttpFoundation\Response;
+use App\Models\AmbassadorTypeNetwork;
+
+use EchoIt\JsonApi\Exception as ApiException;
+use EchoIt\JsonApi\Request as ApiRequest;
+use EchoIt\JsonApi\Handler as ApiHandler;
+use Request;
+
+/**
+* Handles API requests for AmbassadorTypeNetwork
+*/
+class AmbassadorTypeNetworkHandler extends ApiHandler
+{
+	const ERROR_SCOPE = 1026;
+
+	/**
+	 * List of relations that can be included in response.
+	 */
+	protected static $exposedRelations = [];
+
+	/**
+		* Handles GET requests.
+		* @param EchoIt\JsonApi\Request $request
+		* @return EchoIt\JsonApi\Model|Illuminate\Support\Collection|EchoIt\JsonApi\Response|Illuminate\Pagination\LengthAwarePaginator
+	 */
+	public function handleGet(ApiRequest $request)
+	{
+		//you can use the default GET functionality, or override with your own
+		return $this->handleGetDefault($request, new AmbassadorTypeNetwork);
+	}
+
+	/**
+	 * Handles PUT requests.
+	 * @param EchoIt\JsonApi\Request $request
+	 * @return EchoIt\JsonApi\Model|Illuminate\Support\Collection|EchoIt\JsonApi\Response
+	 */
+	public function handlePut(ApiRequest $request)
+	{
+		//you can use the default PUT functionality, or override with your own
+		return $this->handlePutDefault($request, new AmbassadorTypeNetwork);
+	}
+}
---
+++ app/Handlers/Models/ContainerEnvHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Handlers\Models;
+
+// use Symfony\Component\HttpFoundation\Response;
+use App\Models\ContainerEnv;
+
+use EchoIt\JsonApi\Exception as ApiException;
+use EchoIt\JsonApi\Request as ApiRequest;
+use EchoIt\JsonApi\Handler as ApiHandler;
+use Request;
+
+/**
+* Handles API requests for ContainerEnv
+*/
+class ContainerEnvHandler extends ApiHandler
+{
+	const ERROR_SCOPE = 1028;
+
+	/**
+	 * List of relations that can be included in response.
+	 */
+	protected static $exposedRelations = [];
+
+	/**
+		* Handles GET requests.
+		* @param EchoIt\JsonApi\Request $request
+		* @return EchoIt\JsonApi\Model|Illuminate\Support\Collection|EchoIt\JsonApi\Response|Illuminate\Pagination\LengthAwarePaginator
+	 */
+	public function handleGet(ApiRequest $request)
+	{
+		//you can use the default GET functionality, or override with your own
+		return $this->handleGetDefault($request, new ContainerEnv);
+	}
+
+	/**
+	 * Handles PUT requests.
+	 * @param EchoIt\JsonApi\Request $request
+	 * @return EchoIt\JsonApi\Model|Illuminate\Support\Collection|EchoIt\JsonApi\Response
+	 */
+	public function handlePut(ApiRequest $request)
+	{
+		//you can use the default PUT functionality, or override with your own
+		return $this->handlePutDefault($request, new ContainerEnv);
+	}
+}
