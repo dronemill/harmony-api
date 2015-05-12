417168089a16b98ae18733990f76941684a7c3d4
Patrick McCarren <patrick@mmsc.io>
2015-05-11 23:32:47 -0400

    Seeds.ContainerVolume: updated entryPoint

--- database/seeds/ContainerSeeder.php
+++ database/seeds/ContainerSeeder.php
@@ -9,13 +9,14 @@ class ContainerSeeder extends Seeder {
 		$this->SmartTruncate('App\Models\Container');
 
 		Container::create(array(
-			'id'         => '16461407840577296064',
-			'machine_id' => '5722763259656441665',
-			'name'       => 'super_awesome_container',
-			'hostname'   => 'super_awesome_container',
-			// 'restart' => 'on-failure:3',
-			'image'      => 'ubuntu',
-			'enabled'    => 1,
+			'id'          => '16461407840577296064',
+			'machine_id'  => '5722763259656441665',
+			'name'        => 'super_awesome_container',
+			'hostname'    => 'super_awesome_container',
+			// 'restart'  => 'on-failure:3',
+			'image'       => 'ubuntu',
+			'enabled'     => 1,
+			'entry_point' => '/usr/bin/top'
 		));
 	}
 
